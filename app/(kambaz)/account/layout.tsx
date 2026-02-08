e580adb6bb327a7c51a14f381f559a165d92e40d
Kwon Lok Young <kwonlokyoung@gmail.com>
2026-02-07 20:05:13 -0500

    Semiworking navbar

--- app/(kambaz)/account/layout.tsx
+++ app/(kambaz)/account/layout.tsx
@@ -1,25 +1,33 @@
 import { ReactNode } from "react";
 import KambazNavigation from "../Navigation";
 import AccountNavigation from "./Navigation";
-export default function AccountLayout({ children }: Readonly<{ children: ReactNode }>) {
- return (
-   <table>
-     <tbody>
-       <tr>
-         <td valign="top" width="200">  <KambazNavigation /> </td>
-         <td valign="top" width="100%">
-           <div id="wd-kambaz">
-             <table>
-               <tbody>
-                 <tr>
-                   <td valign="top">              <AccountNavigation /> </td>
-                   <td valign="top" width="100%"> {children}            </td>
-                 </tr>
-               </tbody>
-             </table>
-           </div>
-         </td>
-       </tr>
-     </tbody>
-   </table>
-);}
+export default function AccountLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td valign="top" width="100%">
+            <div id="wd-kambaz">
+              <table>
+                <tbody>
+                  <tr>
+                    <td valign="top">
+                      {" "}
+                      <AccountNavigation />{" "}
+                    </td>
+                    <td valign="top" width="100%">
+                      {" "}
+                      {children}{" "}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
